4118c43ff167e362b6686fd29caaa0c01d1e5423
Celtuzed <uter222222@gmail.com>
2023-02-21 22:21:44 +0300

    еженедельник

---
+++ NoteBook/NoteBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    internal class NoteBook
+    {
+        DateTime dateTime;
+        string Nameof;
+        string Description;
+       
+        public NoteBook(string nameof, string descrition, DateTime date)
+        {
+            Nameof = nameof;
+            Description = descrition;
+            dateTime = date;
+        }
+    }
+}
